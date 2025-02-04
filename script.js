4f832863a34ab9564ddcb43d91bd5ce740979839
Relffor <123898239+Relffor@users.noreply.github.com>
2025-02-04 15:42:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ const messages = [
 ];
 
 let messageIndex = 0;
-let scaleFactor = 1; // Kezdeti zoom érték
 
 function handleNoClick() {
     const noButton = document.querySelector('.no-button');
@@ -22,21 +21,7 @@ function handleNoClick() {
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
     yesButton.style.fontSize = `${currentSize * 1.5}px`;
 }
-    // Ellenőrizzük, hogy a gomb nagyobb-e már, mint a kijelző egy bizonyos része
-    checkAndZoomOut(yesButton);
-}
 
-function checkAndZoomOut(yesButton) {
-    const screenWidth = window.innerWidth;
-    const screenHeight = window.innerHeight;
-    const buttonRect = yesButton.getBoundingClientRect();
-
-    if (buttonRect.width > screenWidth * 0.5 || buttonRect.height > screenHeight * 0.5) {
-        scaleFactor *= 0.9; // Kicsit csökkentjük a zoomot (10%-kal)
-        document.body.style.transform = `scale(${scaleFactor})`;
-        document.body.style.transformOrigin = "center center"; // Középről zoomoljon ki
-    }
-}
 
 function handleYesClick() {
     window.location.href = "yes_page.html";
